0bccd78b872aab0c8e7ae034c5843bd738417bd3
pateldipal <dipalpatel21993@gmail.com>
2020-10-01 18:06:39 +0530

    package install as per requirement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {GallaryComponent} from './gallary/gallary.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {HomeComponent} from './home/home.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'gallary', component: GallaryComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: '**', component: PagenotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import {HomeComponent} from './home/home.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GallaryComponent,
+    PortfolioComponent,
+    PagenotfoundComponent,
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
